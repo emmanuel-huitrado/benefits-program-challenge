112e6b596fa95863856b2d75ce5066c93032d803
luis orozco <jlorozco.1025@gmail.com>
2023-02-09 10:51:27 -0600

    fixing logic for DiscountMastercard

--- src/main/java/coe/unosquare/benefits/order/DiscountMastercard.java
+++ src/main/java/coe/unosquare/benefits/order/DiscountMastercard.java
@@ -18,24 +18,13 @@ public class DiscountMastercard implements Discount{
      */
     public double calculateDiscount(PaymentType paymentType, int numOfProducts, double subtotal){
         double discount;
-        if (paymentType == PaymentType.VISA) {
-            if (numOfProducts >= 10) {
-                discount = 0.15;
-            } else if (numOfProducts >= 7) {
-                discount = 0.10;
-            } else {
-                discount = 0.05;
-            }
-        } else if (paymentType == PaymentType.MASTERCARD) {
-            if (subtotal >= 100) {
-                discount = 0.17;
-            } else if (subtotal >= 75) {
-                discount = 0.12;
-            } else {
-                discount = 0.08;
-            }
+
+        if (subtotal >= 100) {
+            discount = 0.17;
+        } else if (subtotal >= 75) {
+            discount = 0.12;
         } else {
-            discount = 0.0;
+            discount = 0.08;
         }
         return discount;
     }
